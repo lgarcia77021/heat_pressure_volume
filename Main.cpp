874c2e6c0383b043ff5ff81b5be41fe6a4ec7c05
lgarcia77021 <l_garcia77021@outlook.com>
2021-08-08 22:54:46 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string substance;
+    
+    
+    cout << "Please enter substance" << endl;
+    
+    getline(cin,substance);
+    
+    cout << substance << endl;
+    
+ 
+ 
+ return 0;
+}
